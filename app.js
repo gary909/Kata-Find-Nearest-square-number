8e7aab1b598c9f87888967f75afe877ff266cc00
gary909 <garywhite61@hotmail.com>
2023-03-16 13:13:52 +0100

    code

---
+++ app.js
@@ -0,0 +1,9 @@
+function nearestSq(n){
+    // your code
+}
+
+console.log(nearestSq(1)); // 1 "nearestSq(1) = 1"
+console.log(nearestSq(2)); // 1 "nearestSq(1) = 1"
+console.log(nearestSq(10)); // 9 "nearestSq(10) = 9"
+console.log(nearestSq(111)); // 121 "nearestSq(111) = 121"
+console.log(nearestSq(9999)); // 10000 "nearestSq(9999) = 100000"
